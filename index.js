89481aad79496db7a7f810c59d7777eda7858d52
unknown <sajeda1605akter@gmail.com>
2022-11-30 15:19:33 +0600

    'user-api-create'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 require("dotenv").config();
+const jwt=require('jsonwebtoken')
 const cors=require('cors')
 const { MongoClient, ServerApiVersion,ObjectId} = require('mongodb');
 
@@ -25,7 +26,10 @@ async function run(){
     try{
         const categoryCollection = client.db("style-world").collection("categories");
         const productsCollection = client.db("style-world").collection("products");
-        const bookedCollection=client.db('style-world').collection('bookedProduct')
+        const bookedCollection=client.db('style-world').collection('booking')
+        const userCollection=client.db('style-world').collection('users')
+        const productAddCollection=client.db('style-world').collection('addProduct')
+        
         // second hand category
         app.get("/category", async (req, res) => {
             let query = {}
@@ -45,18 +49,66 @@ async function run(){
           })
           
           // book item create
-          app.post('/booked',async(req,res)=>{
+          app.post('/booking',async(req,res)=>{
             const product=req.body;
             const bookingProduct=await bookedCollection.insertOne(product);
             res.send(bookingProduct)
           })
           
-          // product add
+          // product add api create
           app.post('/addProduct',async(req,res)=>{
             const product=req.body;
-            const addProduct=await productsCollection.insertOne(product);
+            const addProduct=await productAddCollection.insertOne(product);
             res.send(addProduct)
           })
+
+          // 
+          app.get('/addProduct',async(req,res)=>{
+            const email=req.body.email
+            const query={email:email}
+             const cursor=productAddCollection.find(query);
+             const addProduct=await cursor.toArray();
+            res.send(addProduct)
+          })
+
+            // data get for my order
+    app.get('/booking',async(req,res)=>{
+      
+  
+     const email=req.body.email
+     const query={email:email}
+      const cursor=bookedCollection.find(query);
+      const reviews=await cursor.toArray();
+      res.send(reviews);
+    })
+
+    // jwt
+    app.get('/jwt',async(req,res)=>{
+      const email=req.query.email;
+      const query={email:email}
+      const user=await userCollection.findOne(query)
+      if(user){
+        const token=jwt.sign({email},process.env.ACCESS_TOKEN_KEY,{expiresIn:'7d'})
+      return  res.send({accessToken:token})
+
+      }
+    
+      res.status(403).send({accessToken:'token'})
+    })
+    //user info
+    app.post('/users',async(req,res)=>{
+      const query=req.body;
+      const addUser=await userCollection.insertOne(query);
+      res.send(addUser)
+    })  
+
+  //   app.delete('/users/:id',async(req,res)=>{
+  //     const id=req.params.id;
+  //     const query={_id:ObjectId(id)}
+  //     const result=await userCollection.deleteOne(query)
+  //     res.send(result)
+
+  // })
 
     }
     finally{
